84bdcdc41adecfb0798aff1415d4205a03fa2c34
Josh Greig <josh.greig@gmail.com>
2023-05-30 23:01:37 -0400

    added Delphi source files

---
+++ bitPro.dpr
@@ -0,0 +1,13 @@
+program bitPro;
+
+uses
+  Forms,
+  bitU in 'bitU.pas' {Form1};
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
